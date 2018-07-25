ed5484b8281845078f6d6f79a68996a2112b74ec
Toader, Sebastian <st0ad3r@gmail.com>
2018-07-25 14:36:04 +0200

    Use EKS Cloud Formation templates from configurable location

--- config/configuration.go
+++ config/configuration.go
@@ -9,11 +9,15 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/spf13/viper"
+	"path/filepath"
 )
 
 const (
 	// local helm path
 	helmPath = "helm.path"
+
+	// the location to get EKS Cloud Formation templates from
+	EksTemplateLocation = "eks.templateLocation"
 )
 
 //Init initializes the configurations
@@ -84,6 +88,8 @@ func init() {
 	viper.SetDefault("monitor.enabled", false)
 	viper.SetDefault("monitor.configmap", "")
 	viper.SetDefault("monitor.mountpath", "")
+
+	viper.SetDefault(EksTemplateLocation, filepath.Join(pwd, "templates", "eks"))
 
 	// Find and read the config file
 	if err := viper.ReadInConfig(); err != nil {
